4f2b59492dd5d0ca267e777ea2eb005c20766dfd
mostafizurRahaman <fahim654326@gmail.com>
2023-06-20 08:10:18 +0600

    feat: promptopia header section is included

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,18 @@
 const Home = () => {
    return (
-      <div>
-         <h1 className="text-2xl font-bold text-primary-orange">
-            Home page is ready to show
+      <section className="w-full flex-col flex-center">
+         <h1 className="head_text text-center">
+            Discover & Share
+            <br className="max-md:hidden" />
+            <span className="orange_gradient text-center">
+               AI-powered prompts
+            </span>
          </h1>
-      </div>
+         <p className="desc text-center">
+            Promptopia is an open-source AI prompting tool for modern world to
+            discover, create and share creative prompts
+         </p>
+      </section>
    );
 };
 
